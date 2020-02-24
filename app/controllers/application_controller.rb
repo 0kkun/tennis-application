1487820818b386c52b6da70ab0a162ff0dcc08f4
0kkun <shinji.okumoto.01@gmail.com>
2020-02-24 16:28:46 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception #意図してない、ユーザからのリクエストをブロックする
+
+  #:authenticate_user!は、ユーザがログインしていない場合はログインページにリダイレクトさせる
+  # before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
