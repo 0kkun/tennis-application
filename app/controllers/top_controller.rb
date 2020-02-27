d1d8b0e03cda8dc12c18fd82856304ebd741e048
0kkun <shinji.okumoto.01@gmail.com>
2020-02-27 18:19:05 +0900

    Update tour schedule function

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -12,6 +12,19 @@ class TopController < ApplicationController
     # @elementsというクラス変数に、search_urlのトップページにあるタグを検索
     # 要素を配列形式で入れている。ビューに渡す用。
     @elements = page.search('#cntAreaL02 li a')
+
+    schedule_search_url = "https://news.tennis365.net/news/tour/schedule/"
+    schedule_agent = Mechanize.new
+    schedule_page = schedule_agent.get(schedule_search_url)
+    @date_elements = schedule_page.search('td.small_day')
+    @match_elements = schedule_page.search('td.small_name')
+    @category_elements = schedule_page.search('td.cate')
+
+    x = schedule_page.search('td.small_day')
+    y = schedule_page.search('td.small_name')
+    z = schedule_page.search('td.cate')
+    @array = [1,2]
+
   end
 
 end
