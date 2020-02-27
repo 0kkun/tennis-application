f002d91810c8b039306a12c120ca56705f53a1c7
0kkun <shinji.okumoto.01@gmail.com>
2020-02-27 22:19:34 +0900

    Amount ranking function

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,29 +1,35 @@
 class TopController < ApplicationController
 
   def index
-    search_url = "http://news.tennis365.net/news/today/"
+    # --------------大会日程のスクレイピング--------------------
+    news_search_url = "http://news.tennis365.net/news/today/"
 
     # agentという変数にMechanizeクラスのインスタンスを代入。
-    agent = Mechanize.new
+    news_agent = Mechanize.new
     # pageという変数にsearch_urlのウェブサイトからHTML情報をgetというメソッドで、
     # Mechanizeインスタンスから抜き出したものを代入
-    page = agent.get(search_url)
+    news_page = news_agent.get(news_search_url)
 
     # @elementsというクラス変数に、search_urlのトップページにあるタグを検索
     # 要素を配列形式で入れている。ビューに渡す用。
-    @elements = page.search('#cntAreaL02 li a')
+    @news_elements = news_page.search('#cntAreaL02 li a')
 
+    # --------------大会日程のスクレイピング--------------------
     schedule_search_url = "https://news.tennis365.net/news/tour/schedule/"
     schedule_agent = Mechanize.new
     schedule_page = schedule_agent.get(schedule_search_url)
+
     @date_elements = schedule_page.search('td.small_day')
     @match_elements = schedule_page.search('td.small_name')
     @category_elements = schedule_page.search('td.cate')
 
-    x = schedule_page.search('td.small_day')
-    y = schedule_page.search('td.small_name')
-    z = schedule_page.search('td.cate')
-    @array = [1,2]
+    # --------------ランキングのスクレイピング--------------------
+    rank_search_url = "https://news.tennis365.net/news/ranking/?s=head"
+    rank_agent = Mechanize.new
+    rank_page = rank_agent.get(rank_search_url)
+    @rank_name_elements = rank_page.search('#rightCol .rankTblB a')
+    @rank_elements = rank_page.search('#rightCol td.num')
+    @point_elements = rank_page.search('#rightCol td.point')
 
   end
 
