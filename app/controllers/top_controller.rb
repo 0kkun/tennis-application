c4982cfb39529dbdd54d5ebfaeb003aa196b979f
0kkun <shinji.okumoto.01@gmail.com>
2020-02-27 13:21:23 +0900

    Function amount

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,10 +3,15 @@ class TopController < ApplicationController
   def index
     search_url = "http://news.tennis365.net/news/today/"
 
+    # agentという変数にMechanizeクラスのインスタンスを代入。
     agent = Mechanize.new
+    # pageという変数にsearch_urlのウェブサイトからHTML情報をgetというメソッドで、
+    # Mechanizeインスタンスから抜き出したものを代入
     page = agent.get(search_url)
+
+    # @elementsというクラス変数に、search_urlのトップページにあるタグを検索
+    # 要素を配列形式で入れている。ビューに渡す用。
     @elements = page.search('#cntAreaL02 li a')
-    @favorite_player = ["フェデラー","ジョコビッチ","ジョコ","ナダル"]
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,27 +3,46 @@
 
 User.create!(name: "okkun", email: "test@gmail.com", password: "11111111")
 
+Favorite.create!(
+  [
+    {
+      user_id: 1, player_id: 1,
+    },
+    {
+      user_id: 1, player_id: 3,
+    }
+  ]
+)
+
 Player.create!(
   [
     {
       name:  'Rafael Nadal',
       name_jp:  'ラファエル・ナダル',
       name_etc: 'R.Nadal',
+      name_search_1: 'ナダル',
+      name_search_2: 'ラファ',
     },
     {
       name:  'Roger Federer',
       name_jp:  'ロジャー・フェデラー',
       name_etc: 'R.Federer',
+      name_search_1: 'フェデラー',
+      name_search_2: 'ロジャー',
     },
     {
       name:  'Novak Djokovic',
       name_jp:  'ノバク・ジョコビッチ',
       name_etc: 'N.Djokovic',
+      name_search_1: 'ジョコビッチ',
+      name_search_2: 'ジョコ',
     },
     {
       name:  'Daniil Medvedev',
       name_jp:  'ダニル・メドベージェフ',
       name_etc: 'D.Medvedev',
+      name_search_1: 'メドヴェージェフ',
+      name_search_2: 'ダニエル',
     }
   ]
 )
