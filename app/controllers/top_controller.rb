3c46c44752e66f3ac38ddd86ac9da36beabd81c8
0kkun <shinji.okumoto.01@gmail.com>
2020-02-27 12:52:32 +0900

    amount function
    お気に入り選手はひとまずコントローラで定義
    このあとにデータベースに登録されている選手からデータを読み出して検索するようにする

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,12 @@
 class TopController < ApplicationController
+
+  def index
+    search_url = "http://news.tennis365.net/news/today/"
+
+    agent = Mechanize.new
+    page = agent.get(search_url)
+    @elements = page.search('#cntAreaL02 li a')
+    @favorite_player = ["フェデラー","ジョコビッチ","ジョコ","ナダル"]
+  end
+
 end
